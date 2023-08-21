17f936b0e9aec02e69efbab120a4ff5d00a6cebc
renehm74 <142324279+renehm74@users.noreply.github.com>
2023-08-21 13:37:44 -0400

    Add files via upload

---
+++ Agenda.java
@@ -0,0 +1,54 @@
+
+/**
+ * AGENDA
+ */
+import java.util.ArrayList;
+public class Agenda 
+{
+    private ArrayList<Nota> lista;
+    {
+        lista = new ArrayList<Nota>();
+    }
+    public void addNota(Nota nota)
+    {
+        lista.add(nota);
+    }
+    public void listarNotas()
+    {
+        if(lista.size()>0){
+            for(Nota item : lista){ 
+                item.mostrar();
+            }
+        }
+        else{
+            System.out.println("--------------------------");
+            System.out.println("No hay notas en la agenda");
+            System.out.println("--------------------------");
+        }
+    }
+    public void contarNotas()
+    {
+        System.out.println("--------------------------");
+        System.out.println("En la agenda existen  "+ lista.size() +" notas");
+        System.out.println("--------------------------");
+    }
+    
+    public void borrarNota(int id)
+    {
+        int pos = -1;
+        boolean flag = false;
+        
+        if(lista.size() > 0){
+            pos++;
+            for (Nota item :lista){
+                if (item.getId() == id){
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag){
+                lista.remove(pos);
+            }
+        }
+    }
+}
